d8a55360b79daef145776e18e06c75365933b7a9
rajat <manohar.rajat9@gmail.com>
2023-10-09 12:17:19 +0530

    Initial Commit

---
+++ src/main/java/com/itvedant/petstoreprac/PetstorepracApplication.java
@@ -0,0 +1,13 @@
+package com.itvedant.petstoreprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstorepracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetstorepracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itvedant/petstoreprac/PetstorepracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itvedant.petstoreprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstorepracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
